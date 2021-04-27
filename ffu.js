e821f85f0ea2a0b56bd8d98d3e8a7b8c96e4d350
Jefferson Scher <jscher2000@gmail.com>
2021-04-27 14:08:15 -0700

    Catch-up upload April 2021

--- ffu.js
+++ ffu.js
@@ -2,6 +2,6 @@ window.onload = function(e){
 	var homelink = document.querySelector('#logo:not([nolink])');
 	if (homelink){
 		homelink.style.cursor = 'pointer';
-		homelink.addEventListener('click', function(){window.location.href='https://jscher2000.github.io/Firefox-File-Utilities/'});
+		homelink.addEventListener('click', function(){window.location.href='https://www.jeffersonscher.com/ffu/'});
 	}
 }
